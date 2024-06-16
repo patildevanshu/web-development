aef307a4d4098b482e24cb1716f17d97fe435ade
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2024-06-16 09:41:22 +0530

    js

---
+++ js/7/app.js
@@ -0,0 +1,46 @@
+// const student = {
+//     name: "devanshu patil",
+//     age : 20,
+//     eng: 88 ,
+//     math : 99, 
+//     phy : 80,
+//     avg() {
+//         let avg = (this.eng + this.math + this.phy)/3
+//         console.log(avg)
+//     }
+// }
+
+
+// console.log("hello1");
+// console.log("hello2");
+// try{
+//     console.log(a);
+// }catch{
+//     console.log("try&catch");
+// }
+// console.log("hello3");
+// console.log("hello4");
+
+// // arrow function
+
+// const sum = (a , b ) => {
+//      return a+b    
+// };
+
+// const mul = (a , b ) => (a*b);
+
+
+// console.log("hello there!");
+
+// setTimeout( ()=> {console.log("Devanshu patil")} ,4000 );
+
+// console.log("Welcome to js!");
+
+
+let id = setInterval( ()=> {console.log("Devanshu patil")} ,2000 );
+console.log(id);
+
+let id2 = setInterval( ()=> {console.log("hello_world")} ,3000 );
+console.log(id2);
+
+// to stop this there is a command {clearInterval(id);}
