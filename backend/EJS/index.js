63df3b39edb9e18764632dad8004992253b7cb07
Devanshu Patil <devanshupatil34@gmail.com>
2024-07-01 10:17:07 +0530

    data.json

--- backend/EJS/index.js
+++ backend/EJS/index.js
@@ -10,6 +10,26 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
+app.get('/rolladice' , (req , res) => {
+  let rolldice = Math.floor(Math.random()*6)+1;
+  res.render("rolldice" , {num : rolldice});
+});
+
 app.get('/', (req, res) => {
   res.render("home.ejs");
 });
+
+
+// app.get('/ig/:username' , (req , res) =>{
+//   let followers = ["devanshu" , "patil" , "unkonown" , "abc"];
+//   let { username } = req.params;
+//   res.render("instagram.ejs" , { username , followers })
+// })
+
+app.get('/ig/:username' , (req , res) =>{
+  let { username } = req.params;
+  let instadata = require("./data.json")
+  let data = instadata[username];
+  
+  res.render("instajsondata.ejs" , { data })
+})
