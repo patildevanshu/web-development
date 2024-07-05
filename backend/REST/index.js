5d51c46dcc28b03441b2fa635ebc84ce4d7b2bf6
DEVANSHU PATIL <152208944+patildevanshu@users.noreply.github.com>
2024-07-05 18:33:36 +0530

    .

--- backend/REST/index.js
+++ backend/REST/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
@@ -11,20 +12,26 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let port = 3000;
 
+
+
 let posts = [
     {
+        id : uuidv4(),
         username : 'devnashu patil',
         content : 'This is my first blog post',
     },
     {
+        id : uuidv4(),
         username : 'yash patil',
-        content : 'great things take time!',
+        content : 'great things take time!'
     },
     {
+        id : uuidv4() ,
         username : 'aadi patil',
-        content : 'do something',
+        content : 'do something'
     },
     {
+        id : uuidv4() ,
         username : 'legend patil',
         content : 'Time to live',
     }
@@ -37,3 +44,30 @@ app.listen(port, () => {
 app.get('/posts', (req, res) => {
   res.render("index.ejs" , { posts })
 });
+
+app.get('/posts/new', (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post('/posts', (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id , username, content });
+    // res.render("index.ejs", { posts });
+    res.redirect('/posts');
+});
+
+app.get('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("show.ejs" , { post })
+  });
+
+app.patch('/posts/:id' , (req , res) => {
+    let { id } = req.params;
+    let newContent = req.body.content ;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(newContent);
+    res.send("working")
+})
